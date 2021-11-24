18890a1673446cc83a6d10d4a7c9352e14a74cbd
Oscar Rolando Bernard Peralta <2492944860101@ingenieria.usac.edu.gt>
2021-11-24 14:23:49 -0600

    Usando fetch para consumir API

--- buscador/src/views/search/index.js
+++ buscador/src/views/search/index.js
@@ -1,15 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import SearchBox from "./components/SearchBox/index";
 
-import data from "../../data/users.json"
 import "./style.css"
 import SearchResult from "./components/SearchResults";
 
 export default function Buscar() {
     
     const [isAtTop, setIsAtTop] = useState(false)
-    const [userData, setUserData] = useState(data)
+    const [userData, setUserData] = useState([])
     const [results, setResults] = useState([])
+
+    useEffect(() => {
+        const getUsers = async() => {
+            const response = await fetch("https://jsonplaceholder.typicode.com/users")
+            const data = await response.json()
+            setUserData(data)
+        }
+        getUsers().catch(null)
+    }, [])
 
     const handleCloseSearch = () => {
         setIsAtTop(false)
