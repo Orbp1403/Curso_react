a001d6ea6c8ea401fe7ab81387075f00462f084e
Oscar Rolando Bernard Peralta <2492944860101@ingenieria.usac.edu.gt>
2021-11-25 04:44:19 -0600

    Prueba de axios

--- buscador/src/views/search/index.js
+++ buscador/src/views/search/index.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import axios from "axios";
 import SearchBox from "./components/SearchBox/index";
 
 import "./style.css"
@@ -12,9 +13,14 @@ export default function Buscar() {
 
     useEffect(() => {
         const getUsers = async() => {
-            const response = await fetch("https://jsonplaceholder.typicode.com/users")
-            const data = await response.json()
-            setUserData(data)
+            try{
+                /*const response = await fetch("https://jsonplaceholder.typicode.com/users")
+                const data = await response.json()*/
+                const {data} = await axios.get("https://jsonplaceholder.typicode.com/users")
+                setUserData(data)
+            }catch(err){
+                console.log(err)
+            }
         }
         getUsers().catch(null)
     }, [])
