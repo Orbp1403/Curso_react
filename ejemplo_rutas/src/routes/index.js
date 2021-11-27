6d288627870b50d3acdad8d061d12c176f494656
Oscar Rolando Bernard Peralta <2492944860101@ingenieria.usac.edu.gt>
2021-11-27 12:54:39 -0600

    Agregando un scroll to top

--- ejemplo_rutas/src/routes/index.js
+++ ejemplo_rutas/src/routes/index.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route, Routes as Rutas } from "react-router-dom";
+import ScrollToTop from "../components/ScrollToTop";
 import Cuatrocientoscuatro from "../views/404";
 import Home from "../views/Home";
 import PokeDetail from "../views/PokeDetail";
@@ -6,6 +7,7 @@ import PokeDetail from "../views/PokeDetail";
 export default function Routes(){
     return (
         <Router>
+            <ScrollToTop />
             <Rutas>
                 <Route exact path="/" element={<Home />} />
                 <Route exact path="/pokemon/:id" element={<PokeDetail />} />
