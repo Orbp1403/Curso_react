3d931576fec920daf54792795ba5d93829b3f622
Oscar Rolando Bernard Peralta <2492944860101@ingenieria.usac.edu.gt>
2021-11-27 12:42:47 -0600

    Obtener y mostrar los detalles

--- ejemplo_rutas/src/context/pokemon/Provider.js
+++ ejemplo_rutas/src/context/pokemon/Provider.js
@@ -6,20 +6,44 @@ import apiCall from "../../api"
 export default function PokemonProvider({ children }) {
     
     const [pokemon, setPokemon] = useState([])
+    const [pokemonDetail, setPokemonDetail] = useState({})
+    const [isLoading, setIsLoading] = useState(false)
 
     const getPokemon = async () => {
         try {
+            setIsLoading(true)
             const pokemonResult = await apiCall({
                 url : "https://pokeapi.co/api/v2/pokemon?limit=100"
             })
             setPokemon(pokemonResult.results)
         }catch(err){
             setPokemon([])
+        }finally{
+            setIsLoading(false)
+        }
+    }
+
+    const getPokemonDetail = async (id) => {
+        if(!id) Promise.reject("Id es requerido")
+        try{
+            setIsLoading(true)
+            const pokemonDetail = await apiCall({
+                url : `https://pokeapi.co/api/v2/pokemon/${id}`
+            })
+            setPokemonDetail(pokemonDetail)
+        }catch(err){
+            setPokemonDetail({})
+        } finally {
+            setIsLoading(false)
         }
     }
 
     return(
-        <PokemonContext.Provider value={{ getPokemon, pokemon }}>
+        <PokemonContext.Provider value={{ getPokemon, 
+        pokemon, 
+        getPokemonDetail,
+        pokemonDetail,
+        isLoading }}>
             {children}
         </PokemonContext.Provider>
     );
--- ejemplo_rutas/src/views/PokeDetail/index.js
+++ ejemplo_rutas/src/views/PokeDetail/index.js
@@ -1,8 +1,31 @@
+import { useContext, useEffect } from "react"
 import { useParams } from "react-router"
 
+import PokemonContext from "../../context/pokemon"
+
 export default function PokeDetail () {
-    let { id } = useParams()
+    const { id } = useParams()
+    const { getPokemonDetail, pokemonDetail, isLoading } = useContext(PokemonContext)
+
+    useEffect(() => {
+        /** 
+         * Cada que cambie el id
+         * cambie el detalle del pokemon
+        */
+        getPokemonDetail(id).catch(null)
+    }, [])
+
+    console.log(pokemonDetail)
+    if(isLoading){
+        return (<p>
+            Cargando pokemon...
+        </p>)
+    }
     return (
-        <div>PokeDetail</div>
+        <div>
+            <p>{`Nombre: ${pokemonDetail?.name}`}</p>
+            <p>{`Peso: ${pokemonDetail?.weight} kg.`}</p>
+            <p>{`Altura: ${pokemonDetail?.height} cm.`}</p>
+        </div>
     )
 }
